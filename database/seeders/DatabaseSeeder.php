6741108a17629638f21943c5c5110ba154cd7c60
Berk Akın <berkakin99@hotmail.com>
2023-03-19 17:32:22 +0300

    quiz mig oluştu. Factory ve seed yapısı değişti.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::insert([
-            'name'=> 'Berk Akın',
-            'email' => 'berkakin@hotmail.com',
-            'email_verified_at' => now(),
-            'type' => 'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::Class,
         ]);
-
-         \App\Models\User::factory(5)->create();
     }
 }
