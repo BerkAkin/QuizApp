c8598df095e68fd940358d2b353ad8cfdcc5f4f8
Berk Akın <berkakin99@hotmail.com>
2023-03-27 01:18:51 +0300

    feat:sınav güncelleme eklendi
    hata ekranları, sınav güncelleme ekranı dizayn edildi ve sınav güncelleme işlemleri yazıldı

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Quiz;
 use App\Http\Requests\QuizCreateRequest;
+use App\Http\Requests\QuizUpdateRequest;
 
 class QuizController extends Controller
 {
@@ -64,7 +65,8 @@ class QuizController extends Controller
      */
     public function edit($id)
     {
-        //
+        $quiz = Quiz::find($id) ?? abort(404,'Böyle bir sınav mevcut değil');
+        return view('admin.quiz.edit', compact('quiz'));
     }
 
     /**
@@ -74,9 +76,11 @@ class QuizController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(QuizUpdateRequest $request, $id)
     {
-        //
+        $quiz = Quiz::find($id) ?? abort(404,'Güncellenecek böyle bir sınav mevcut değil');
+        Quiz::where('id',$id)->update($request->except(['_method','_token']));
+        return redirect()->route('quizzes.index')->withSuccess('Sınav Güncelleme Başarılı');
     }
 
     /**
--- resources/views/admin/quiz/edit.blade.php
+++ resources/views/admin/quiz/edit.blade.php
@@ -1,30 +1,31 @@
 <x-app-layout>
-    <x-slot name="header">Sınav Oluştur</x-slot>
+    <x-slot name="header">Sınavı Güncelle</x-slot>
 
     <div class="card">
         <div class="card-body">
-            <form action="{{route('quizzes.store')}}" method="POST">
+            <form action="{{route('quizzes.update', $quiz->id)}}" method="POST">
+            @method('PUT')
              @csrf
              <div class="form-group">
                 <label class="fw-bold" for="title">Sınav Başlığı</label>
-                <input type="text" name="title" class="form-control" required value="{{old('title')}}">
+                <input type="text" name="title" class="form-control" required value="{{$quiz->title}}">
             </div>
             <div class="form-group mt-3">
                 <label class="fw-bold" for="description">Sınav Açıklaması</label>
-                <textarea name="description" class="form-control" rows="4" >{{old('description')}}</textarea>
+                <textarea name="description" class="form-control" rows="4">{{$quiz->description}}</textarea>
             </div>
 
             <div class="form-check mt-3">
-                <input class="form-check-input" @if(old('finished_at'))checked @endif type="checkbox"  id="isFinished">
+                <input class="form-check-input" @if($quiz->finished_at) checked @endif type="checkbox"  id="isFinished">
                 <label class="form-check-label" for="isFinished">Bitiş Tarihi Var Mı?</label>
               </div>
-            <div id="finished" @if(!old('finished_at'))  style="display: none" @endif class="form-group mt-3">
+            <div id="finished" @if(!$quiz->finished_at)  style="display: none" @endif class="form-group mt-3">
                 <label class="fw-bold" for="finished_at">Bitiş Tarihi</label>
-                <input type="datetime-local" name="finished_at" class="form-control" value="{{old('finished_at')}}">
+                <input type="datetime-local" name="finished_at" class="form-control" value="{{$quiz->finished_at}}">
             </div>
 
             <div class="d-grid gap-2 mt-4">
-                <button type="submit" class="btn btn-success btn-sm fs-5">Sınav Yarat</button>
+                <button type="submit" class="btn btn-success btn-sm fs-5">Sınavı Güncelle</button>
             </div>
 
             </form>
