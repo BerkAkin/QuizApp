6fd46d9b678e74eee83e91e4eba1539a639336cb
Berk Akın <berkakin99@hotmail.com>
2023-03-28 23:57:14 +0300

    feat: soru factory oluşturuldu

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Question;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Question::class;
+    public function definition()
+    {
+        return [
+            'quiz_id'=>rand(1,10),
+            'question'=>$this->faker->sentence(rand(3,7)),
+            'answer1'=>$this->faker->sentence(rand(1,3)),
+            'answer2'=>$this->faker->sentence(rand(1,3)),
+            'answer3'=>$this->faker->sentence(rand(1,3)),
+            'answer4'=>$this->faker->sentence(rand(1,3)),
+            'answer5'=>$this->faker->sentence(rand(1,3)),
+            'correct_answer'=>'answer'.rand(1,5)
+        ];
+    }
+}
