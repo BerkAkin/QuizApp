22a96121def37a87a903729218879b12582a1a28
Berk Akın <berkakin99@hotmail.com>
2023-04-05 00:20:36 +0300

    feat: sorular artık güncellenebiliyor

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -6,38 +6,30 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Quiz;
 use App\Http\Requests\QuestionCreateRequest;
+use App\Http\Requests\QuestionUpdateRequest;
 use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
+
     public function index($id)
     {
         $quiz = Quiz::whereId($id)->with('questions')->first() ?? abort(404, 'Sınav Mevcut Değil');
         return view('admin.question.list', compact('quiz'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function create($id)
     {
         $quiz = Quiz::find($id);
         return view('admin.question.create', compact('quiz'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function store(QuestionCreateRequest $request, $id)
     {
         if ($request->hasFile('image')) {
@@ -52,46 +44,43 @@ class QuestionController extends Controller
         return redirect()->route('questions.index', $id)->withSuccess('Soru Başarıyla Oluşturuldu');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
+
     public function show($quiz_id, $id)
     {
         return $quiz_id . "- " . $id;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+
+
+    public function edit($quiz_id, $question_id)
     {
-        //
+
+        $question = Quiz::find($quiz_id)->questions()->whereId($question_id)->first() ?? abort(404, 'Sınav veya Soru Mevcut Değil');
+        return view('admin.question.edit', compact('question'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+
+
+
+    public function update(QuestionUpdateRequest $request, $quiz_id, $question_id)
     {
-        //
+        if ($request->hasFile('image')) {
+            $dosyaAdi = Str::slug($request->question) . "." . $request->image->extension();
+            $dosyaAdiYeni = 'uploads/' . $dosyaAdi;
+            $request->image->move(public_path('uploads'), $dosyaAdi);
+            $request->merge([
+                'image' => $dosyaAdiYeni,
+            ]);
+        }
+        Quiz::find($quiz_id)->questions()->whereId($question_id)->first()->update($request->post());
+        return redirect()->route('questions.index', $quiz_id)->withSuccess('Soru Başarıyla Güncellendi');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
+
     public function destroy($id)
     {
         //
