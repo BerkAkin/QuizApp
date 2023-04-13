1bdb18ba8cc278480129ee0daaf86392899f88d8
Berk Akın <berkakin99@hotmail.com>
2023-04-13 23:56:34 +0300

    feat: dashboarda quizler eklendi

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Quiz;
+
+class MainController extends Controller
+{
+    public function Dashboard()
+    {
+        $quizzes = Quiz::where('status', 'published')->WithCount('questions')->paginate(5);
+        return view('dashboard', compact('quizzes'));
+    }
+
+    public function quizDetail($slug)
+    {
+        $quiz = Quiz::whereSlug($slug)->withCount('questions')->first() ?? abort(404, 'Sınav Bulunamadı');
+        return view('quizDetail', compact('quiz'));
+    }
+}
