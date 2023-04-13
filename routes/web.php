836873911010fbc203173a28e4b91936f188718b
Berk Akın <berkakin99@hotmail.com>
2023-04-14 00:01:34 +0300

    feat: sınava girme rotaları açıldı

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\Admin\QuestionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\QuizController;
+use App\Http\Controllers\MainController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'verified'])->get('/panel', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('panel', [MainController::class, 'Dashboard'])->name('dashboard');
+    Route::get('quiz/{slug}', [MainController::class, 'quizDetail'])->name('quiz.detail');
+});
 
 
 Route::group(['middleware' => ['auth', 'isAdmin'], 'prefix' => 'admin'], function () {
