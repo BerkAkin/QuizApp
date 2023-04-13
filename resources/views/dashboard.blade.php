04b4900c3e87da0f4d19d9475fa746cd6e109548
Berk Akın <berkakin99@hotmail.com>
2023-04-14 00:00:28 +0300

    feat: sınavlar listeleniyor ve detayına gidiliyor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,30 @@
 <x-app-layout>
     <x-slot name="header">Anasayfa</x-slot>
 
-    <div class="alert alert-danger">Bootstrap Alert Mesajı!</div>
+<div class="row">
+    <div class="col-md-8">
+        <div class="list-group">
+            @foreach($quizzes as $quiz)
+            <a href="{{route('quiz.detail', $quiz->slug)}}" class="list-group-item list-group-item-action" aria-current="true">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1 text-dark">{{$quiz->title}}</h5>
+                <small class="text-primary bold fw-bold">{{$quiz->finished_at ? $quiz->finished_at->diffForHumans() . " bitiyor": null}}</small>
+            </div>
+            <p class="mb-1 text-muted">{{Str::limit($quiz->description,150)}}</p>
+            <small>Soru Sayısı: {{$quiz->questions_count}}</small>
+            </a>
+            @endforeach
+            <div class="mt-3">
+                {{$quizzes->links()}}
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="col-md-4"></div>
+</div>
+    
+
+
 </x-app-layout>
