38e92480eb36b4de06e30aad88d5a58679e8f5e1
Berk Akın <berkakin99@hotmail.com>
2023-04-24 17:35:53 +0300

    feat: sınav sonucu hesaplama uygulandı

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use App\Models\Answer;
+use App\Models\Result;
 
 class MainController extends Controller
 {
@@ -27,7 +29,33 @@ class MainController extends Controller
 
     public function result(Request $request, $slug)
     {
-        return $request->post();
+        $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404, 'Sınav Mevcut Değil');
+        $correct = 0;
+        foreach ($quiz->questions as $question) {
+            /*  echo $question->id . '-' . $question->correct_answer . '/' . $request->post($question->id) . '<br>'; */
+            Answer::create([
+                'user_id' => auth()->user()->id,
+                'question_id' => $question->id,
+                'answer' => $request->post($question->id)
+            ]);
+
+            if ($question->correct_answer === $request->post($question->id)) {
+                $correct += 1;
+            }
+        }
+        $score = round((100 / count($quiz->questions)) * $correct);
+        $wrong =  count($quiz->questions) - $correct;
+        Result::create([
+            'user_id' => auth()->user()->id,
+            'quiz_id' => $quiz->id,
+            'score' => $score,
+            'correct' => $correct,
+            'wrong' => $wrong,
+
+        ]);
+
+        return redirect()->route('quiz.detail',$quiz->slug)->withSuccess('Sınav tamamlandı');
+
     }
 
 
