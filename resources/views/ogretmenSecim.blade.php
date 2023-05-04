93f6e24c3b15abd2df57410131a4a5a2ef9b314b
Berk Akın <berkakin99@hotmail.com>
2023-05-05 02:49:31 +0300

    feat: öğrenci id bilgisi eklendi

--- resources/views/ogretmenSecim.blade.php
+++ resources/views/ogretmenSecim.blade.php
@@ -1,14 +1,16 @@
 <x-app-layout>
     <x-slot name="header">Öğretmen Seçimi</x-slot>
-
-    <div class="row ">
+<form action="{{route('ogretmen.store')}}" method="POST">
+    @csrf
+    <div class="row">
         <div class="col-12">
             <h5 class="float-right">
-                <a href="" class="btn btn-md btn-dark">Seçimi Kaydet</a>
+                <button type="submit" class="btn btn-md btn-dark">Seçimi Kaydet</button>
             </h5>
         </div>
     </div>
-    <div class="row mt-3">
+    <div class="row mt-3"> 
+        <input name="ogrenci_id" type="text" value="{{auth()->user()->id}}" class="d-none">
         @foreach($ogretmenler as $item)
             <div class="col-md-2 d-flex justify-content-center">
                 <div class="card" style="width: 18rem;">
@@ -16,15 +18,17 @@
                         <img class="card-img-top" src="{{asset('storage/')}}/{{$item->profile_photo_path}}">
                     @endif
                     <div class="card-body text-center">
-                        <input class="form-check-input" type="radio" name="secim" id="ogretmenId">
+                        <input class="form-check-input" type="radio" name="ogretmen_id" id="{{$item->id}}" value="{{$item->id}}">
                         <label class="form-check-label" for="secim">
                         {{$item->name}}  
                         </label>
+                       
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
+</form>
     
 
 
