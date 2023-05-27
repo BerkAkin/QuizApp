611a4af9fa022537e3b827757490af8e86b25255
Berk Akın <berkakin99@hotmail.com>
2023-05-27 22:36:04 +0300

    feat: yönetici paneli hazırlandı

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -79,7 +79,7 @@
 
         <div class="col-md-12 ">
 
-            <div>
+                <div>
                     <ul class="nav nav-tabs border-warning" id="myTab" role="tablist">
                         <li class="nav-item" role="presentation">
                             <button class="nav-link active fw-bold" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Gelen Mesajlar  
@@ -190,6 +190,128 @@
 
             </div>
     </div>
+@elseif(auth()->user()->type=='ustYonetici')
+
+
+
+
+
+
+
+
+
+
+    <div class="row">
+        <div class="col-md-8">
+                <div>
+                    <ul class="nav nav-tabs border-warning" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active fw-bold" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Gelen Mesajlar  
+                                @if($yeniler!="0")
+                                <span class="badge bg-success rounded-pill"> {{$yeniler}}</span>
+                                @endif
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link fw-bold" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Gönderilmiş Mesajlar</button>
+                        </li>
+                        <li class="nav-item ml-auto"> <a data-bs-toggle="modal" data-bs-target="#staticBackdrop" class=" btn btn-warning fw-bold"><i class="fa-solid fa-pencil"></i></a></li>
+                    </ul>
+                </div>
+                <div class="tab-content " id="myTabContent">
+                    {{-- Gelen Mesajlar --}}
+                    <div class="tab-pane fade show active " id="home" role="tabpanel" aria-labelledby="home-tab">
+                            
+                            <!--MESAJLAR BAŞLANGIÇ-->
+                                <div class="card border rounded-0 " style="width: 100%;">
+                                
+                                    <div style="max-height: 600px; overflow-y:scroll">
+                                        @foreach($userMessages as $item)
+                                            <div class="accordion bg-light" style="margin: 0.5px" id="accordionExample">
+                                                <div class="accordion-item  border-bottom-0 border-start-0 border-end-0 rounded-0">
+                                                    <h2 class="accordion-header" id="headingOne">
+                                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
+                                                            <span class=" w-100  fw-bold text-capitalize @if($item->okundu_bilgisi==1) text-dark @else text-success @endif">{{Str::limit($item->baslik,25)}}
+                                                                @if($item->okundu_bilgisi==0)<span class="float-end badge bg-success rounded-pill"> Yeni</span> @endif 
+
+                                                            </span>          
+                                                        </button>
+                                                    </h2>
+                                                    
+                                                    <div id="collapse{{$item->id}}" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                        <div class="accordion-body">
+                                                            <div class="text-muted">{{$item->mesaj}}</div>
+                                                            <hr>
+                                                            <div>
+                                                                <div class=" badge mt-3 ms-0 bg-info rounded-pill text-dark">Gönderen: {{$item->name}}</div>
+                                                                <div class="float-end badge mt-3 ms-0 bg-warning rounded-pill text-dark">{{$item->created_at->diffForHumans()}}</div>
+                                                            </div>
+                                                            
+                                                            @if($item->okundu_bilgisi==0)
+                                                                <a href="{{route('okundu', $item->id)}}" class="mt-4 btn btn-sm btn-success w-100 fw-bold"><i class="fa-regular fa-envelope-open"></i> Okundu Olarak İşaretle</a>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            <!--MESAJLAR BİTİŞ-->
+                    </div>
+                    {{-- Giden Mesajlar --}}
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="card border rounded-0 " style="width: 100%;">
+                                <div style="max-height: 600px; overflow-y:scroll">
+                                    @foreach($gidenMesajlar as $item)
+                                        <div class="accordion bg-light" style="margin: 0.5px" id="accordionExample">
+                                            <div class="accordion-item  border-bottom-0 border-start-0 border-end-0 rounded-0">
+                                                <h2 class="accordion-header" id="headingOne">
+                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
+                                                        <span class=" w-100  fw-bold text-capitalize text-dark  text-success ">{{Str::limit($item->baslik,25)}}
+                                                            <span class="float-end">
+                                                            @if($item->okundu_bilgisi=="0") 
+                                                                <i class="fa-solid fa-envelope"></i>
+                                                            @else 
+                                                                <i class="fa-solid fa-envelope-open"></i>
+                                                            @endif
+                                                            </span>
+                                                        </span>          
+                                                    </button>
+                                                </h2>
+                                                <div id="collapse{{$item->id}}" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                    <div class="accordion-body">
+                                                        <div class="text-muted">{{$item->mesaj}}</div>
+                                                        <hr>
+                                                        <div>
+                                                            <div class=" badge mt-3 ms-0 bg-info rounded-pill text-dark">Gönderilen Kişi: {{$item->name}}</div>
+                                                            <div class="float-end badge mt-3 ms-0 bg-warning rounded-pill text-dark">{{$item->created_at->diffForHumans()}}</div>
+                                                            
+        
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
 @else
 {{-- Ana Kısım --}}
     <div class="row">
@@ -386,19 +508,58 @@
                 <span class="input-group-text bg-warning"><i class="fa-regular fa-message text-lg"></i></span>
                 <textarea minlength="10" maxlength="300" required name="mesaj" rows="6" style="resize: none;" placeholder="İçerik" class="form-control" aria-label="With textarea"></textarea>
               </div>
-              @if(auth()->user()->type=="admin")
+                @if(auth()->user()->type=="admin")
 
-                <div class="input-group mt-3">
-                    <label class="input-group-text bg-warning" for=""><i class="fa-regular fa-user text-lg"></i></label>
-                    <select required name="ogrenci"  class="form-select" aria-label="Filter select" style="border-color: rgb(192, 191, 191); box-shadow: none;">
-                    <option selected></option>
+                    <div class="input-group mt-3">
+                        <label class="input-group-text bg-warning" for=""><i class="fa-regular fa-user text-lg"></i></label>
+                        <select required name="ogrenci"  class="form-select" aria-label="Filter select" style="border-color: rgb(192, 191, 191); box-shadow: none;">
+                        <option selected></option> 
+                        @foreach($yoneticiler as $kullanici)
+                        <option value="{{$kullanici->id}}">{{$kullanici->name}}</option>
+                        @endforeach
+                        @foreach($kullanicilar as $kullanici)
+                        <option value="{{$kullanici->id}}">{{$kullanici->name}}</option>
+                        @endforeach
+                    
 
-                    @foreach($kullanicilar as $kullanici)
-                    <option value="{{$kullanici->id}}">{{$kullanici->name}}</option>
-                    @endforeach
+                        </select>
+                    </div>
 
-                    </select>
+                @endif
+
+                @if(auth()->user()->type=="ustYonetici")
+
+                    <div class="input-group mt-3">
+                        <label class="input-group-text bg-warning" for=""><i class="fa-regular fa-user text-lg"></i></label>
+                        <select required name="ogrenci"  class="form-select" aria-label="Filter select" style="border-color: rgb(192, 191, 191); box-shadow: none;">
+                        <option selected></option>
+
+                        @foreach($ogretmenler as $kullanici)
+                        <option value="{{$kullanici->id}}">{{$kullanici->name}}</option>
+                        @endforeach
+
+                        </select>
+                    </div>
+
+                @endif
+
+                @if(auth()->user()->type=="user")
+                <div class="form-check mt-3 ms-2">
+                    <input class="form-check-input bg-warning border-1 border-warning" style="box-shadow:none !important; transform:scale(1.2)" type="checkbox" id="goster">
+                    <label class="form-check-label text-sm fw-bold" for="flexCheckDefault" >Admine Gönder</label>
+                    <label class="form-check-label text-sm fw-bold" for="flexCheckDefault">(Öğretmene Göndermek İçin Boş Bırakın)</label>
                 </div>
+                    <div id="adminGoster" class="input-group mt-3" style="display: none">
+                        <label class="input-group-text bg-warning" for=""><i class="fa-regular fa-user text-lg"></i></label>
+                        <select  name="adminId"  class="form-select" aria-label="Filter select" style="border-color: rgb(192, 191, 191); box-shadow: none;">
+                        <option value="" selected></option>
+                    
+                        @foreach($yoneticiler as $kullanici)
+                        <option value="{{$kullanici->id}}">{{$kullanici->name}}</option>
+                        @endforeach
+
+                        </select>
+                    </div>
 
                 @endif
          
@@ -419,6 +580,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
 
+
+    <script>
+        $('#goster').change(function() {
+            if($('#goster').is(':checked')){
+                $('#adminGoster').show();
+            }
+            else{
+                $('#adminGoster').hide();
+            }
+        })
+    </script>
 </x-slot>
 
 </x-app-layout>
